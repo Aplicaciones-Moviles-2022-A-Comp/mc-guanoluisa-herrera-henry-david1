6c9e84a8d70d5e48b89593d4d5a004fad8594234
Guanoluisa Henry <henry.guanoluisa@epn.edu.ec>
2022-07-17 23:30:04 -0500

    Proyecto_Primer_Bimestre_Henry_Guanoluisa

--- MyApplicationH/app/src/main/java/com/example/myapplicationh/MainActivity.kt
+++ MyApplicationH/app/src/main/java/com/example/myapplicationh/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.example.myapplicationh
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import io.sentry.Sentry;
+import java.lang.Exception;
+import io.sentry.SentryLevel
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main
+        setContentView(R.layout.activity_main)
+                //Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
+                Sentry.captureMessage("testing SDK setup", SentryLevel.INFO)
 
-            val = botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+            val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
         botonCicloVida
-            .setOnClickListener{
+            .setOnClickListener {
                 irActividad(ACicloVida::class.java)
-
             }
     }
- fun irActividad (
-
-     clase: Class <*>
- ){
-
-     vale intent = Intent(this, clase)
-     startActivity (intent)
- }
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
+    }
 
 
 }
